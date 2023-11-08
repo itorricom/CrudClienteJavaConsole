fdd0bf72e17130ad4e92979541d3800e0a97f11e
Isidoro Torrico M <itorrico.m@gmail.com>
2023-11-08 08:43:18 -0400

    Leer, Eliminar, Actualizar

--- src/ClienteService.java
+++ src/ClienteService.java
@@ -20,14 +20,37 @@ public class ClienteService {
     }
 
     public  static void listarClientes(){
-
+        ClienteDAO.leerClienteDB();
     }
 
     public static void borrarCliente(){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Indica el ID del cliente a borrar: ");
+        int id_cliente = sc.nextInt();
+        ClienteDAO.borrarCliente(id_cliente);
     }
 
     public static void editarCliente(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escribe nuevo nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Escribe nuevo email: ");
+        String email = sc.nextLine();
+
+        System.out.print("Escribe nuevo telefono: ");
+        String telefono = sc.nextLine();
+
+        System.out.print("Indica el ID del cliente a editar: ");
+        int id_cliente = sc.nextInt();
+
+        Cliente acliente = new Cliente();
+        acliente.setNombre(nombre);
+        acliente.setEmail(email);
+        acliente.setTelefono(telefono);
+        acliente.setId(id_cliente);
+
+        ClienteDAO.actualizarCliente(acliente);
 
     }
 }
